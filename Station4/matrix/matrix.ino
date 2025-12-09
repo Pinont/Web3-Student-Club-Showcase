8a3694a8462afba300a544929536603c7c684538
PinozenTH <pinozen.thailand@gmail.com>
2025-12-09 21:13:57 +0700

    station 4 paper to matrix complete

--- Station4/matrix/matrix.ino
+++ Station4/matrix/matrix.ino
@@ -5,51 +5,34 @@
 
 //ใส่ MAC Address
 uint8_t paperAddress[]  = {0x08, 0xF9, 0xE0, 0xF6, 0x23, 0x58}; // M5Paper
-uint8_t stickc1Address[] = {0x00, 0x4B, 0x12, 0xC4, 0x2D, 0xF8}; // StickC Plus2
-uint8_t stickc2Address[] = {0x00, 0x4B, 0x12, 0xC4, 0x35, 0x48};
+uint8_t stickcAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // StickC Plus2
 uint8_t echoAddress[]   = {0x90, 0x15, 0x06, 0xFA, 0xE7, 0x70}; // Echo
 
-const int INITIAL_BALANCE = 5;
-
-struct MenuItem {
-    const char* name;
-    int price;
-};
-
-const MenuItem menuList[] = {
-    {"Invalid", 0},
-    {"Coffee", 2},
-    {"Croissant", 3},
-    {"Lunch Set", 5},
-    {"Tea", 2}
-};
-//try
 // โครงสร้างข้อมูล
-typedef struct struct_menu_id {
-    uint8_t menuID;    
-} struct_menu_id;
-struct_menu_id incomingMenu;
-
 typedef struct struct_order {
-    char menuName[32];
-    int price;
-    int newBalance;
+    char menuName[32]; 
+    int price;         
 } struct_order;
-struct_order outgoingOrder;
+
+struct_order incomingOrder; 
+
+bool massSend = false;
 
 int standbyIcon[] = { 0, 1, 2, 3, 4, 5, 9, 10, 12, 14, 15, 19, 20, 21, 22, 23, 24 };
+
 // ไอคอนติ๊กถูก
 int checkIcon[] = { 15, 21, 17, 13, 9 };
 
+bool continueSend = false;
+
 void showStandbyPattern() {
     M5.dis.clear();
-    for (int i = 0; i < (sizeof(standbyIcon) / sizeof(standbyIcon[0])); i++) M5.dis.drawpix(standbyIcon[i], 0x0000FF);
+    for (int i = 0; i < 17; i++) M5.dis.drawpix(standbyIcon[i], 0x0000FF);
 }
 
 void showSuccessPattern() {
     M5.dis.clear();
-    // ต้องครอบการหารทั้งหมดด้วยวงเล็บเดียว (sizeof(array) / sizeof(element))
-    for (int i = 0; i < (sizeof(checkIcon) / sizeof(checkIcon[0])); i++){}//สีเขียว
+    for (int i = 0; i < 5; i++) M5.dis.drawpix(checkIcon[i], 0x00FF00); //สีเขียว
 }
 
 void showProcessingPattern() {
@@ -57,71 +40,45 @@ void showProcessingPattern() {
     M5.dis.fillpix(0xFFFF00); //สีเหลืองทั้งจอ คือ กำลังส่ง
 }
 
-
-
 void addPeer(uint8_t *macAddr) {
     esp_now_peer_info_t peerInfo;
     memset(&peerInfo, 0, sizeof(peerInfo));
     memcpy(peerInfo.peer_addr, macAddr, 6);
     peerInfo.channel = 1;
     peerInfo.encrypt = false;
-    esp_now_del_peer(macAddr);
     esp_now_add_peer(&peerInfo);
 }
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    Serial.print("Last Packet Send Status: ");
-    if (status == ESP_NOW_SEND_SUCCESS) {
-        Serial.println("Delivery Success");
-    } else {
-        Serial.println("Delivery Fail");
-
-        showStandbyPattern(); 
-    }
-}
 //ฟังก์ชันทำงานเมื่อได้รับข้อมูลกลับมาจากPaper
 void OnDataRecv(const esp_now_recv_info_t * info, const uint8_t *incomingData, int len) {
-    //ตรวจสอบว่าเป็นข้อมูล Order ไหม
-    if (len == 1) {
-        uint8_t id= incomingData[0];
 
-        if (id >= 1 && id <= 4){
-            int cost =menuList[id].price;
-            int newBalance = INITIAL_BALANCE -cost;
-            
-            Serial.printf(" Received Order from Paper: Menu: %s, Price: %d\n", id, menuList[id].name, cost);
-             
-            if (newBalance >= 0){
-                strcpy(outgoingOrder.menuName, menuList[id].name);
-                outgoingOrder.price = cost;
-                outgoingOrder.newBalance = newBalance;
+    // Print first received byte and match from list
+    // 49 = 1st choice
+    // 50 = 2nd choice
+    // 51 = 3rd choice
+    // 51 = 4th choice
 
-                Serial.printf("   - New Balance: %d CCoin\n", outgoingOrder.newBalance);
+    if (!massSend) return; // stop doing stuff when it's done or not being started
+    Serial.println(incomingData[0]);
 
-                esp_now_send(stickc1Address, (uint8_t *) &outgoingOrder, sizeof(outgoingOrder));
-                esp_now_send(stickc2Address, (uint8_t *) &outgoingOrder, sizeof(outgoingOrder));
-        //สั่ง Echo ให้ร้องเตือน
-                uint8_t triggerSignal = 1;
-                esp_now_send(echoAddress, &triggerSignal, sizeof(triggerSignal));
+    // send data to stick c and handle coin moderation
+    esp_now_send(stickcAddress, &incomingData[0], sizeof(incomingData[0]));
+    uint8_t triggerSignal = 1 ;
+    esp_now_send(echoAddress, &triggerSignal, sizeof(triggerSignal));
 
-        //แสดงติ๊กถูกสีเขียว
-                showSuccessPattern();
-                delay(2000);
-                showStandbyPattern();
-            
-            }else{
-                Serial.printf("Insufficient Balance. Need %d, have %d. Transaction rejected.\n", cost, INITIAL_BALANCE);
-                M5.dis.fillpix(0xFF0000);
-                delay(2000);
-                showStandbyPattern();
-            }
-        }else {
-            Serial.printf("Received Invalid data size from MAC: %02X.", info->src_addr[0]);
-            M5.dis.fillpix(0xFF00FF);
-            delay(1000);
-            showStandbyPattern();
-        }
-    } 
+    showSuccessPattern();
+    delay(2000); //โชว์ค้างไว้ 2 วินาที
+    showStandbyPattern(); //กลับไปเป็นสีน้ำเงิน
+    massSend = false; // stop mass sending because it's successfully sent and got feedback
+} 
+
+void massSendUntilReceive() {
+    massSend = true;
+    while (massSend) {
+        //ส่ง Requestไปหา Paper  ตอนนี้ส่งแค่สัญญาณเปล่าๆ
+        uint8_t requestSignal = 1 ;
+        esp_now_send(paperAddress, &requestSignal, sizeof(requestSignal));
+    }
 }
 
 void setup() {
@@ -130,21 +87,15 @@ void setup() {
     WiFi.mode(WIFI_STA);
     esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
 
-    if (esp_now_init() != ESP_OK){
-      Serial.println("ESP-NOW Init Failed! Restarting....");
-      delay(2000);
-      ESP.restart();
-    }
-
-    esp_now_register_recv_cb(OnDataRecv);
-    esp_now_register_send_cb(OnDataSent);
+    if (esp_now_init() != ESP_OK) return;
     
     addPeer(paperAddress);
-    addPeer(stickc1Address);
-    addPeer(stickc2Address);
+    addPeer(stickcAddress);
     addPeer(echoAddress);
 
-    Serial.println("Atom-Matrix System Ready");
+    esp_now_register_recv_cb(OnDataRecv);
+
+    Serial.println("System Ready");
     showStandbyPattern(); //สีน้ำเงิน
 }
 
@@ -154,13 +105,10 @@ void loop() {
     if (M5.Btn.wasPressed()) {
         
         //แสดงสีเหลือง ส่งข้อมูลไปถาม Paper
-        showProcessingPattern(); 
+        showProcessingPattern();
 
-        //ส่ง Requestไปหา Paper  ตอนนี้ส่งแค่สัญญาณเปล่าๆ
-        uint8_t requestSignal = 1 ;
-        esp_now_send(paperAddress, &requestSignal, sizeof(requestSignal));
-        
-        Serial.println("Sent Request to Paper(Asking for Order Data)...");
+        Serial.println("Sent Request to Paper...");
+
+        massSendUntilReceive();
     }
-    delay(10);
 }
---
+++ Station4/paper/paper.ino
@@ -0,0 +1,243 @@
+#include <M5EPD.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <HTTPClient.h>
+#include <esp_now.h>
+#include "config.h"
+
+// สร้าง Canvas 2 ใบ (ใบใหญ่=เมนู, ใบเล็ก=Status)
+M5EPD_Canvas canvas(&M5.EPD);        
+M5EPD_Canvas status_canvas(&M5.EPD); 
+M5EPD_Canvas choice_canvas(&M5.EPD); 
+
+uint8_t atomMAC[] = {0x4C, 0x75, 0x25, 0xAC, 0xBE, 0x18};
+
+typedef struct struct_message {
+  char msg[32];
+} struct_message;
+
+struct_message outgoing;
+
+int selectedChoice = 0;
+bool submitted = false;
+AsyncWebServer server(80);
+
+struct Activity {
+    String name;
+};
+
+Activity activities[] = {
+    {"Coffee       2 CCoin"},
+    {"Croissant    3 CCoin"},
+    {"Lunch Set    5 CCoin"},
+    {"Tea          2 CCoin"}
+};
+
+void sendCommand(const char *cmd);
+void updateStatus(String msg);
+
+// Receive trigger action from atom matrix
+void onDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  // --- TRIGGER EVENT ON M5PAPER ---
+  Serial.println("receive from matrix");
+  if (selectedChoice != 0) {
+      String msg = String(selectedChoice);
+      sendCommand(msg.c_str());
+      updateStatus("Sending Order: " + msg);
+  } else {
+      updateStatus("Please Select Order");
+  }
+}
+
+// Send number of order to atom matrix
+void sendCommand(const char *cmd) {
+  strcpy(outgoing.msg, cmd);
+  esp_now_send(atomMAC, (uint8_t *)&outgoing, sizeof(outgoing));
+  Serial.print("Sent: ");
+  Serial.println(cmd);
+}
+
+void drawMenu() {
+
+    canvas.createCanvas(540, 960); 
+    
+    // Header
+    canvas.setTextSize(4);
+    canvas.drawString("CAMT WEB3 CAFE", 100, 50);
+
+    canvas.setTextSize(3);
+
+    int yRect = 130;
+    int yString = 155;
+    for (int i = 1; i <= 4; i++) {
+        canvas.drawRect(0, yRect, 540, 80, 15);
+        String name = String(i) + ". " + activities[i-1].name;
+        canvas.drawString(name, 30, yString);
+        defaultSelectButton(i);
+        yRect += 100;
+        yString += 100;
+    }
+    
+    canvas.setTextColor(15, 0); 
+
+    canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
+}
+
+void updateStatus(String msg) {
+    status_canvas.createCanvas(540, 100); 
+    status_canvas.fillCanvas(0);          
+    status_canvas.setTextSize(3);
+    status_canvas.drawString(msg, 20, 20);
+
+    status_canvas.pushCanvas(0, 700, UPDATE_MODE_DU4); 
+}
+
+void sumbitStatus(String msg1, String msg2) {
+    status_canvas.createCanvas(540, 100); 
+    status_canvas.fillCanvas(0);          
+    status_canvas.setTextSize(3);
+    status_canvas.drawString(msg1, 20, 20);
+    status_canvas.drawString(msg2, 23, 50);
+
+    status_canvas.pushCanvas(0, 700, UPDATE_MODE_DU4);
+
+    Serial.println("Submitted");
+}
+
+void selectButton(int choice) {
+    int ySelector = choice * 100 + 70;
+    canvas.fillCircle(490, ySelector, 30, 15);
+}
+
+void defaultSelectButton(int choice) {
+    int ySelector = choice * 100 + 70;
+    canvas.fillCircle(490, ySelector, 30, 0);
+    canvas.drawCircle(490, ySelector, 30, 15);
+}
+
+void handleSystemReset(AsyncWebServerRequest *request) {
+    drawMenu();
+    updateStatus("");
+    submitted = false;
+    selectedChoice = 0;
+    Serial.println("Received reset signal from Core");
+    request->send(200, "text/plain", "M5-Paper S3 reset complete.");
+}
+
+bool sendReceiveCoin(String coin_value) {
+    HTTPClient http;
+    String url = "http://" + IP_STICKC.toString() + ENDPOINT_EARN_COIN;
+    Serial.println("Sending " + coin_value + " to " + url);
+    http.begin(url);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String postData = "amount=" + coin_value; 
+
+    int code = http.POST(postData);
+    String response = http.getString();
+    http.end();
+    if (code == 200) {
+        Serial.println("Sent OK");
+        return true;
+    }
+    Serial.println(String(code) + " Error: " + response);
+    return false;
+}
+
+void setup() {
+    M5.begin();
+    M5.EPD.SetRotation(90); // Landscape
+    Serial.begin(115200);
+
+    // กำหนด Static IP
+    WiFi.config(IP_PAPER_S4, IP_STATION1_AP, IPAddress(255, 255, 255, 0));
+    WiFi.begin(AP_SSID, AP_PASSWORD);
+
+    Serial.println("Connecting to AP...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.print("\nConnected to AP. IP: ");
+    Serial.println(WiFi.localIP());
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    esp_now_register_recv_cb(onDataRecv);
+
+    // Register peer
+    esp_now_peer_info_t peerInfo;
+    memset(&peerInfo, 0, sizeof(peerInfo));
+    memcpy(peerInfo.peer_addr, atomMAC, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+    
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+    }
+
+    // ตั้งค่า Server Endpoints
+    server.on(ENDPOINT_RESET_GLOBAL, HTTP_POST, handleSystemReset);
+    server.on(ENDPOINT_HEARTBEAT, HTTP_GET, [] (AsyncWebServerRequest *r) {
+        r->send(200, "text/plain", "OK");
+    });
+
+    server.begin();
+
+    drawMenu();
+}
+
+void loop() {
+    M5.update();
+
+    if (M5.TP.available()) {
+        if (!M5.TP.isFingerUp()) {
+            M5.TP.update();
+            
+            // อ่านค่า X (0-540) และ Y (0-960)
+            int x = M5.TP.readFingerX(0);
+            int y = M5.TP.readFingerY(0);
+
+            // Serial.printf("X: %d, Y: %d\n", x, y);
+            if (!submitted) {
+                if (selectedChoice != 1 && x >= 130 && x <= 229) {
+                    if (selectedChoice > 0) {
+                        defaultSelectButton(selectedChoice);
+                    }
+                    selectedChoice = 1;
+                    selectButton(selectedChoice);
+                    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+                }
+                else if (selectedChoice != 2 && x >= 230 && x <= 329) {
+                    if (selectedChoice > 0) {
+                        defaultSelectButton(selectedChoice);
+                    }
+                    selectedChoice = 2;
+                    selectButton(selectedChoice);
+                    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+                }
+                else if (selectedChoice != 3 && x >= 330 && x <= 410) {
+                    if (selectedChoice > 0) {
+                        defaultSelectButton(selectedChoice);
+                    }
+                    selectedChoice = 3;
+                    selectButton(selectedChoice);
+                    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+                }
+                else if (selectedChoice != 4 && x >= 430 && x <= 510) {
+                    if (selectedChoice > 0) {
+                        defaultSelectButton(selectedChoice);
+                    }
+                    selectedChoice = 4;
+                    selectButton(selectedChoice);
+                    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+                }
+            }
+            delay(100);
+        }
+    }
+}
